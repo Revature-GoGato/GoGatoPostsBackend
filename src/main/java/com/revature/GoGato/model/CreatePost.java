c24ba11eb050e98f492a18d53acf87942584a88b
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-04-27 10:50:25 -0400

    created CreatePost class

---
+++ src/main/java/com/revature/GoGato/model/CreatePost.java
@@ -0,0 +1,14 @@
+package com.revature.GoGato.model;
+
+
+import org.springframework.stereotype.Service;
+
+
+public class CreatePost {
+    private int id;
+    private int userid;
+    private String content;
+    private int likes;
+
+
+}
