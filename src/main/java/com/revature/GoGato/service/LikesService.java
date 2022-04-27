761ab4d7c1b8b3f9e26a10e8b5001d4cfc99d2da
JustinSmith <steelindian66@gmail.com>
2022-04-27 11:10:56 -0500

    Finished LikesService

--- src/main/java/com/revature/GoGato/service/LikesService.java
+++ src/main/java/com/revature/GoGato/service/LikesService.java
@@ -1,4 +1,18 @@
 package com.revature.GoGato.service;
 
+import com.revature.GoGato.model.Likes;
+import com.revature.GoGato.repository.LikesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LikesService {
+
+    private final LikesRepository likesRepository;
+    public LikesService(LikesRepository likesRepository){this.likesRepository = likesRepository;}
+
+    public List<Likes> findAllLikes(){return likesRepository.findAll();}
+
+    public Likes createLikes(Likes likes){return likesRepository.save(likes);}
 }
