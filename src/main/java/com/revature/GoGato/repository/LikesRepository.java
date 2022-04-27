f0f5adb3ca59f25d6c06fac3e2d43a17805db7f3
JustinSmith <steelindian66@gmail.com>
2022-04-27 11:06:04 -0500

    fixed repository to be an interface

--- src/main/java/com/revature/GoGato/repository/LikesRepository.java
+++ src/main/java/com/revature/GoGato/repository/LikesRepository.java
@@ -1,7 +1,8 @@
 package com.revature.GoGato.repository;
 
+import com.revature.GoGato.model.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LikesRepository {
-}
+public interface LikesRepository extends JpaRepository<Likes, Integer> { }
