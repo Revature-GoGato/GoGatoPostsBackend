96c7c6a5a7e3e7bdc690be22815d2083da1ec63c
JustinSmith <steelindian66@gmail.com>
2022-04-27 11:19:23 -0500

    Finished LikesController

--- src/main/java/com/revature/GoGato/controller/LikesController.java
+++ src/main/java/com/revature/GoGato/controller/LikesController.java
@@ -1,4 +1,24 @@
 package com.revature.GoGato.controller;
 
+import com.revature.GoGato.model.Likes;
+import com.revature.GoGato.service.LikesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/likes")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LikesController {
+    private LikesService likesService;
+    public LikesController(LikesService likesService){this.likesService=likesService;}
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Likes> findAllLikes(){return likesService.findAllLikes();}
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Likes createLikes(@RequestBody Likes likes){return likesService.createLikes(likes);}
 }
