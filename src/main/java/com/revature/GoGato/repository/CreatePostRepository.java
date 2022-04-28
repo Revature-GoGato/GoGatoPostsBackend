e084c4d82a0254520c34c480b2f0c8ba76756f34
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-04-28 17:11:53 -0400

    Revert "Post create"

--- src/main/java/com/revature/GoGato/repository/CreatePostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.GoGato.repository;
-
-import com.revature.GoGato.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface CreatePostRepository extends JpaRepository<Post, Integer>{}
